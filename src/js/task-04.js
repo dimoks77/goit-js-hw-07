2b9624a797f0ffac7d620f9a5546dc70b399ea2f
Dmytro Kasko <dimoks77@gmail.com>
2023-11-19 20:00:20 +0100

    start

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,24 @@
 
+var form = document.querySelector('.login-form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    var email = form.elements.email;
+    var password = form.elements.password;
+    
+    
+    // console.log(email);
+    // console.log(email.value);
+    
+    if ((email.value === "") || (password.value === "")) { 
+            alert("All form fields must be filled in"); 
+            return
+        }
+
+    name1 = 0
+    name2 = 0
+
+
+
+});
